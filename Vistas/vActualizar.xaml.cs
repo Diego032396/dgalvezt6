f66cfbef2855de06a61f69a8080cc038112341ac
Diego032396 <diegojosuegalvez@gmail.com>
2024-05-14 23:15:51 -0500

    final

--- Vistas/vActualizar.xaml.cs
+++ Vistas/vActualizar.xaml.cs
@@ -5,28 +5,60 @@ namespace dgalvezt6.Vistas;
 
 public partial class vActualizar : ContentPage
 {
-    
+    Estudiante estud;
     public vActualizar(Estudiante datos)
 	{
         
 		InitializeComponent();
-     
-       TxtNombre.Text= datos.nombre.ToString();
-       TxtApellido.Text= datos.apellido.ToString();
-       TxtEdad.Text= datos.edad.ToString();
+        estud = datos;
+        initializer();
 	}
+    private void initializer()
+    {
+        TxtNombre.Text = estud.nombre;
+        TxtApellido.Text = estud.apellido;
+        TxtEdad.Text = estud.edad.ToString() ;
+        Txtcodigo.Text = estud.codigo.ToString();
+    }
+
+
 
     private async void btnEliminar_Clicked(object sender, EventArgs e)
     {
+        try
+        {
+            WebClient cliente = new WebClient();
+            var parametros = new System.Collections.Specialized.NameValueCollection();
+            cliente.UploadValues("http://192.168.0.103:80/appmovil/post.php?codigo=" + Txtcodigo.Text, "DELETE", parametros);
+            DisplayAlert("OK", "Registro Eliminado", "Cerrar");
+            Navigation.PushAsync(new Vistas.vInicio());
 
+        }
+        catch (Exception)
+        {
 
-        Navigation.PushAsync(new Vistas.vInicio());
+            DisplayAlert("Alert", "error", "Cerrar");
+        }
+
+        
     }
 
     private async void tnbActualizar_Clicked(object sender, EventArgs e)
 	{
 
-        Navigation.PushAsync(new Vistas.vInicio());
+        try
+        {
+            WebClient cliente = new WebClient();
+            var parametros = new System.Collections.Specialized.NameValueCollection();
+            cliente.UploadValues("http://192.168.0.103:80/appmovil/post.php?nombre=" + TxtNombre.Text + "&apellido=" + TxtApellido.Text + "&edad=" + TxtEdad.Text + "&codigo=" + Txtcodigo.Text, "PUT", parametros);
+            Navigation.PushAsync(new Vistas.vInicio());
+        }
+        catch (Exception)
+        {
+
+            DisplayAlert("Alert", "error", "Cerrar");
+        }
+
 
     }
 }
